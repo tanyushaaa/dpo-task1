9cf3d936ff94da9de0d59675a4c14cc7c2e1488a
tatiana_zotova <zotovvvat@yandex.ru>
2022-10-09 23:51:19 +0300

    working version

---
+++ task1.py
@@ -0,0 +1,41 @@
+import random
+from datetime import datetime
+
+def time_calc(time_list):
+    def decorator(func):
+        def wrapped(*args, **kwargs):
+            start_time = datetime.now()
+            result = func(*args, **kwargs)
+            end_time = datetime.now()
+            res_time = start_time - end_time
+            time_list.append(res_time.microseconds)
+            return result
+        return wrapped
+    return decorator
+
+
+def fill_array():
+    random_list = []
+    for i in range(1000000):
+        random_list.append(random.randint(0, 999))
+    return random_list
+
+
+time_list = [] #microsec
+@time_calc(time_list)
+def calc_hist(some_list):
+    hist = [0 for i in range(10)]
+    for i in some_list:
+        hist[i // 100] += 1
+    return hist
+
+
+def main():
+    data_list = fill_array()
+    for i in range(100):
+        calc_hist(data_list)
+    print('Minimum time: {:.3f} seconds'.format(min(time_list) / 1000000))
+    print('Maximum time: {:.3f} seconds'.format(max(time_list) / 1000000))
+    print('Average time: {:.3f} seconds'.format(sum(time_list) / len(time_list) / 1000000))
+
+main()
